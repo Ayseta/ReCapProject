fb6ca272c3b1d81c3fc51fda24880c9c58a3f210
Ayseta <77755376+Ayseta@users.noreply.github.com>
2021-03-07 00:19:40 +0300

    Rent a car

---
+++ ReCapProject/Business/Concrete/CarManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager:ICarService
+    {
+        InMemoryCarDal _carDal;
+
+        public CarManager(InMemoryCarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll() 
+        {
+            return _carDal.GetAll();
+        }
+    }
+}
